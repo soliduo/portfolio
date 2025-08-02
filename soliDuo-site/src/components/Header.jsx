3c17fb73b50ded15e44105b4596512c81b6ee815
Sampaio-Luan <luansampaio.dev@gmail.com>
2025-08-02 18:39:26 -0300

    luan

--- soliDuo-site/src/components/Header.jsx
+++ soliDuo-site/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo-container">
-        <h1 className="logo">Three<span className="k-custom">K</span>ode</h1>
+        <h1 className="logo">SoliDuo</h1>
         <p className="slogan">Transformando ideias em experiências digitais.</p>
       </div>
     </header>
